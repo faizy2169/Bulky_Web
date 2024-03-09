b98e0bd6326d254f8cc03062debc03a93ecc86c5
faizy2169 <faizy2169@gmail.com>
2024-03-10 02:14:23 +0500

    Edit Data in Database

--- BulkeyWeb/BulkeyWeb/Controllers/CategoryController.cs
+++ BulkeyWeb/BulkeyWeb/Controllers/CategoryController.cs
@@ -25,9 +25,52 @@ namespace BulkeyWeb.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index","Category");
+            // Server Side Validation
+
+            if (obj.Name != null && obj.Name.ToLower() == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The field value should not be same.");
+            }
+            if (obj.Name == "test")
+            {
+                ModelState.AddModelError("", "Test value is not allowed");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? categoryFrmDb = _db.Categories.Find(id);
+            //Category? categoryFrmDb1 = _db.Categories.FirstOrDefault(item => item.Id == id);
+            //Category? categoryFrmDb2 = _db.Categories.Where(item => item.Id == id).FirstOrDefault();
+            if (categoryFrmDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFrmDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
         }
 
     }
--- BulkeyWeb/BulkeyWeb/Models/Category.cs
+++ BulkeyWeb/BulkeyWeb/Models/Category.cs
@@ -8,8 +8,10 @@ namespace BulkeyWeb.Models
         [Key]
         public int Id { get; set; }
         [DisplayName("Category Name")]
+        [MaxLength(30)]
         public required string Name { get; set; }
         [DisplayName("Category Order")]
+        [Range(1,100, ErrorMessage = "Display Order range should be 1-100")]
         public int DisplayOrder { get; set; }
     }
 }
